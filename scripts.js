05411a0a0cfbf0863cc68c68f41aee8801e128dd
eugeniamariotti-ctrl <eugeniamariotti@gmail.com>
2025-11-25 13:49:31 -0300

    Carga de prueba

---
+++ scripts.js
@@ -0,0 +1,56 @@
+let nombre = "María Eugenia";
+let apellido = "Mariotti";
+let edad = 27;
+let tengoMascota = false;
+
+console.log("Nombre = ", nombre);
+console.log("Apellido = ", apellido);
+console.log("Edad = ", edad);
+console.log("Tengo mascota = ", tengoMascota);
+
+let infoPersonal = {
+    nombre: "María Eugenia",
+    apellido: "Mariotti",
+    edad: 27,
+    tengoMascota: false,
+    comidaFav: ["Hamburguesas", "Lasagna", "Asado"],
+
+};
+
+console.log(infoPersonal);
+console.log(infoPersonal.comidaFav[0]);
+
+let titulo = document.querySelector("h1")
+console.log("Titulo: ", titulo.innerText)
+
+function mostrarLargoNombre(numDeLetras) {
+    return numDeLetras.length;
+}
+
+let largoNombre = mostrarLargoNombre("María Eugenia")
+console.log("El nombre tiene " + largoNombre + " letras")
+
+
+//let boton = document.querySelectorAll("button");
+
+function cambiarColor(colorPersonalizado) {
+    let texto = document.querySelector("h1");
+    //boton.style.backgroundColor = colorPersonalizado;
+
+    texto.style.color = colorPersonalizado;
+}
+
+let listaSuper = ["Pan", "Hamburguesa", "Queso cheddar", "Lechuga", "Cebolla"]
+
+function agregarArticulo() {
+    let texto = window.prompt("Agregar un artículo del super");
+    let loIncluye = listaSuper.includes(texto);
+    if(loIncluye) {
+        alert("Este articulo ya existe");
+    } else {
+        alert("Gracias por su aporte");
+    }
+}
+
+agregarArticulo();
+
